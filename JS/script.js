358d1a6641587d178be37a090d8203c2df1acddd
3ffie <112381528+3ffie@users.noreply.github.com>
2022-10-18 21:59:57 +0200

    added some variabels

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
 // legger tekst her så det blir lastet opp i github
+
+const slideshowImages = document.querySelectorAll('.slideShow__images');
+const prevButton = document.querySelector('.slideShow__button--previous');
+const nextButton = document.querySelector('.slideShow__button--next');
+
+
+
