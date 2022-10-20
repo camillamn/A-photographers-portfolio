3b2b047361d79f80dd8241c759eb1bc30b939ac2
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2022-10-20 11:40:06 +0200

    added queryselector to js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,8 @@
 // legger tekst her så det blir lastet opp i github
 
 const slideshowImages = document.querySelectorAll('.slide');
-
 const prevButton = document.querySelector('.slideShow__button--previous');
 const nextButton = document.querySelector('.slideShow__button--next');
-
 const imageText = document.querySelectorAll('slideshow__images--text');
-
-
+const leftArrow = document.querySelectorAll('slideShow__button--previous');
+const rightArrow = document.querySelectorAll('slideShow__button--next');
