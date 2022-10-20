ebdbb84c42ac37514b1ffecfcbd130b0cb524566
Camilla <112381378+camillamn@users.noreply.github.com>
2022-10-20 17:41:25 +0200

    Removed slideshow-container, added grid

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,22 @@
-// legger tekst her så det blir lastet opp i github
+// // legger tekst her så det blir lastet opp i github
 
-const slideShowImages = document.querySelectorAll('.slide');
-const imagesInSlideshow = document.querySelectorAll('.slideShow__images')
-const prevButton = document.querySelector('.slideShow__button--previous');
-const nextButton = document.querySelector('.slideShow__button--next');
-/* const imageText = document.querySelectorAll('slideshow__images--text'); */
-const navigationDots = document.querySelectorAll('.slideShow__dots');
-const current = 0 ;
+// const slideShowImages = document.querySelectorAll('.slide');
+// const imagesInSlideshow = document.querySelectorAll('.slideShow__images')
+// const prevButton = document.querySelector('.slideShow__button--previous');
+// const nextButton = document.querySelector('.slideShow__button--next');
+// /* const imageText = document.querySelectorAll('slideshow__images--text'); */
+// const navigationDots = document.querySelectorAll('.slideShow__dots');
+// const current = 0 ;
 
 
-nextButton.addEventListener('click',handleNextButtonClick);
+// nextButton.addEventListener('click',handleNextButtonClick);
 
-function handleNextButtonClick() {
-	slide ();
-}
+// function handleNextButtonClick() {
+// 	slide ();
+// }
 
-function slide() {
-	const photoBox = document.querySelectorAll('.photo_box');
-	photoBox.classList.slide('.photo_box__hidden');
-};
+// function slide() {
+// 	const photoBox = document.querySelectorAll('.photo_box');
+// 	photoBox.classList.slide('.photo_box__hidden');
+// };
+
