bf05b9c5b4b5d80b1ee162532e761126456e90ff
Camilla <112381378+camillamn@users.noreply.github.com>
2022-10-20 19:25:33 +0200

    Addes js on next and previous button

--- JS/script.js
+++ JS/script.js
@@ -20,20 +20,43 @@
 // 	photoBox.classList.slide('.photo_box__hidden');
 // };
 
+
+
 const slides = document.querySelectorAll('.photo_box');
 
 slides.forEach((slide, index) => {
 	slide.style.transform = `translateX(${index * 100}%)`;
 });
 
-console.log(slides)
+// next slide
+const nextSlide = document.querySelector('.slideshow__button--next');
 
 let currentSlide = 0;
 
-const nextSlide = document.querySelector('.slideshow__button--next');
+let maxSlide = slides.length - 1;
 
 nextSlide.addEventListener('click', function () {
-	currentSlide++;
+	if (currentSlide=== maxSlide) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
+	}
+
+	slides.forEach((slide, index) => {
+		slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+	});
+});
+
+
+// previous slide
+const previousSlide = document.querySelector('.slideshow__button--previous');
+
+previousSlide.addEventListener('click', function () {
+	if (currentSlide === 0) {
+		currentSlide = maxSlide;
+	} 	else {
+		currentSlide--;
+	}
 
 	slides.forEach((slide, index) => {
 		slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
