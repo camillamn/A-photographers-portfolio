14775fa6c8fd6fa1374c2baa04b097713bed7b72
Camilla <112381378+camillamn@users.noreply.github.com>
2022-10-20 18:51:59 +0200

    Added styling, grid, and js button

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
 // // legger tekst her så det blir lastet opp i github
 
 // const slideShowImages = document.querySelectorAll('.slide');
-// const imagesInSlideshow = document.querySelectorAll('.slideShow__images')
-// const prevButton = document.querySelector('.slideShow__button--previous');
-// const nextButton = document.querySelector('.slideShow__button--next');
+// const imagesInSlideshow = document.querySelectorAll('.slideshow__images')
+// const prevButton = document.querySelector('.slideshow__button--previous');
+// const nextButton = document.querySelector('.slideshow__button--next');
 // /* const imageText = document.querySelectorAll('slideshow__images--text'); */
-// const navigationDots = document.querySelectorAll('.slideShow__dots');
+// const navigationDots = document.querySelectorAll('.slideshow__dots');
 // const current = 0 ;
 
 
@@ -20,3 +20,22 @@
 // 	photoBox.classList.slide('.photo_box__hidden');
 // };
 
+const slides = document.querySelectorAll('.photo_box');
+
+slides.forEach((slide, index) => {
+	slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+console.log(slides)
+
+let currentSlide = 0;
+
+const nextSlide = document.querySelector('.slideshow__button--next');
+
+nextSlide.addEventListener('click', function () {
+	currentSlide++;
+
+	slides.forEach((slide, index) => {
+		slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+	});
+});
