af6cd072f6bb86b129ef3a20b71e15135b9c37b9
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2022-10-20 15:29:00 +0200

    made it into rows

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
 // legger tekst her så det blir lastet opp i github
 
-const slideshowImages = document.querySelectorAll('.slide');
+const slideShowImages = document.querySelectorAll('.slide');
+const imagesInSlideshow = document.querySelectorAll('.slideShow__images')
 const prevButton = document.querySelector('.slideShow__button--previous');
 const nextButton = document.querySelector('.slideShow__button--next');
-const imageText = document.querySelectorAll('slideshow__images--text');
-const leftArrow = document.querySelectorAll('slideShow__button--previous');
-const rightArrow = document.querySelectorAll('slideShow__button--next');
+/* const imageText = document.querySelectorAll('slideshow__images--text'); */
+const navigationDots = document.querySelectorAll('.slideShow__dots');
+const current = 0 ;
 
-console.log(slideshowImages); 
-console.log(imageText);
+
